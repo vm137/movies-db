1475242ac280c412cb1077766e17367ce1349fdf
Victor Mikhaylov <vic.m@mail.ru>
2019-03-12 21:28:38 +0300

    Add dev/prod environments for webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,31 @@
-var webpack = require('webpack');
-var path = require('path');
+let path = require('path');
 
-module.exports = {
-    entry: path.resolve(__dirname, 'src/client/index'),
-    output: {
-        path: __dirname + '/dist',
-        publicPath: '/',
-        filename: 'bundle.js'
-    },
-    devServer: {
-        contentBase: path.resolve(__dirname, 'public')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loaders: ['babel-loader']
-            },
-            {
-                test: /(\.css)$/,
-                loaders: ['style-loader', 'css-loader']
-            }
-        ]
-    },
-    mode: 'production'
+module.exports = env => {
+    console.log('env: ', env);
+
+    return {
+        entry: path.resolve(__dirname, 'src/client/index'),
+        output: {
+            path: __dirname + '/dist',
+            publicPath: '/',
+            filename: 'bundle.js'
+        },
+        devServer: {
+            contentBase: path.resolve(__dirname, 'public')
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loaders: ['babel-loader']
+                },
+                {
+                    test: /(\.css)$/,
+                    loaders: ['style-loader', 'css-loader']
+                }
+            ]
+        },
+        mode: (env === 'prod') ? 'production' : 'development'
+    };
 };
