9bb04a2f74b02b9cf51c22b07848e990331143e5
Victor Mikhaylov <vic.m@mail.ru>
2019-03-19 17:01:44 +0300

    chore: delete fakedata.js

--- src/client/components/SingleMovie/SingleMovie.jsx
+++ src/client/components/SingleMovie/SingleMovie.jsx
@@ -1,24 +1,28 @@
-import React, {Component} from 'react';
-import SearchResults from '../SearchResults/SearchResults';
-
+import React from 'react';
 import './SingleMovie.scss';
 
-export default class SingleMovie extends Component {
+const SingleMovie = () => <h1>SingleMovie</h1>;
 
-    constructor(props) {
-        super(props);
-    }
+// export default class SingleMovie extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       movieId: null,
+//     };
+//   }
+//
+//   render() {
+//     return (
+//       <div>
+//         <h2>Single Movie.</h2>
+//
+//         {this.props.movie}
+//
+//         <SearchResults movies={this.props.movies} />
+//
+//       </div>
+//     );
+//   }
+// }
 
-    render() {
-        return (
-          <div>
-            <h2>Single Movie.</h2>
-
-            {this.props.movie}
-
-            <SearchResults movies={this.props.movies} />
-
-          </div>
-        );
-    }
-}
+export default SingleMovie;
