42b70b46921dc95b0546773364afac19bce49b08
Victor Mikhaylov <vic.m@mail.ru>
2019-04-09 21:58:24 +0300

    test: SearchBlock refactoring

--- src/client/components/SearchBlock/SearchBlock.test.jsx
+++ src/client/components/SearchBlock/SearchBlock.test.jsx
@@ -27,4 +27,37 @@ describe('<SearchBlock />', () => {
     />);
     expect(wrapper.find('.search-block-wrapper').length).toBe(1);
   });
+
+  it('check for state change after click SortBy', () => {
+    const wrapper = mount(<SearchBlock
+      movies={movies}
+      numberFoundMovies={numberFoundMovies}
+      searchCB={searchCB}
+      onClick={onClick}
+    />);
+
+    const button = wrapper.find('.sort-release-date');
+    expect(button.length).toBe(1);
+
+    let status = wrapper.state().sortByRelease;
+    expect(status).toEqual(true);
+    button.simulate('click');
+    status = wrapper.state().sortByRelease;
+    expect(status).toEqual(false);
+  });
+
+  it('check for searchCB (callback)', () => {
+    const onClickJest = jest.fn();
+    const wrapper = mount(<SearchBlock
+      movies={movies}
+      numberFoundMovies={numberFoundMovies}
+      searchCB={onClickJest}
+      onClick={onClick}
+    />);
+
+    const button = wrapper.find('.btn-search');
+    expect(button.length).toBe(1);
+    button.simulate('click');
+    expect(onClickJest.mock.calls.length).toEqual(1);
+  });
 });
