dd5915362b65be6bf886f00f395ccbcb7b3c7714
Victor Mikhaylov <vic.m@mail.ru>
2019-04-15 20:49:48 +0300

    persist

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+// import { store, persistor } from '../store'; ?? it apparently doesn't work
 import './App.scss';
 import ErrorBoundary from './ErrorBoundary';
 import MainWindow from './MainWindow';
 
+
 const App = () => (
   <div className="wrapper">
-    <ErrorBoundary>
-      <MainWindow />
-    </ErrorBoundary>
-
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <ErrorBoundary>
+          <MainWindow />
+        </ErrorBoundary>
+      </PersistGate>
+    </Provider>
     <div className="push" />
   </div>
 );
--- src/client/store.js
+++ src/client/store.js
@@ -1,4 +1,6 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 import thunk from 'redux-thunk';
 import reducer from './reducers/rootReducer';
 
@@ -14,16 +16,24 @@ const initialState = {
   sortBy: true,
 };
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
 /* eslint-disable no-underscore-dangle */
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 /* eslint-enable */
 
 const store = createStore(
-  reducer,
+  persistedReducer,
   initialState,
   composeEnhancers(
     applyMiddleware(thunk),
   ),
 );
+const persistor = persistStore(store);
 
-export default store;
+export default () => ({ store, persistor });
