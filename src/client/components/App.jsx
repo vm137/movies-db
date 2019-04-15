8f9104324464c05707489cbbbccd5a2b88034515
Victor Mikhaylov <vic.m@mail.ru>
2019-04-15 21:21:04 +0300

    feat: persist updates

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
-// import { store, persistor } from '../store'; ?? it apparently doesn't work
+import storeFn from '../store';
 import './App.scss';
 import ErrorBoundary from './ErrorBoundary';
 import MainWindow from './MainWindow';
 
-
 const App = () => (
   <div className="wrapper">
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+    <Provider store={storeFn().store}>
+      <PersistGate loading={null} persistor={storeFn().persistor}>
         <ErrorBoundary>
           <MainWindow />
         </ErrorBoundary>
