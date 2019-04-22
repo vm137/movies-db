d5a1e2717999e9137837759f893ecf16476d5340
Victor Mikhaylov <vic.m@mail.ru>
2019-04-22 18:39:54 +0300

    feat: delete persist functionality

--- src/client/store.js
+++ src/client/store.js
@@ -1,6 +1,4 @@
 import { applyMiddleware, compose, createStore } from 'redux';
-import { persistReducer, persistStore } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 import thunk from 'redux-thunk';
 import reducer from './reducers/rootReducer';
 
@@ -15,25 +13,16 @@ const initialState = {
   sortBy: true,
 };
 
-const persistConfig = {
-  key: 'root',
-  storage,
-};
-
-const persistedReducer = persistReducer(persistConfig, reducer);
-
 /* eslint-disable no-underscore-dangle */
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 /* eslint-enable */
 
 const store = createStore(
-  persistedReducer,
+  reducer,
   initialState,
   composeEnhancers(
     applyMiddleware(thunk),
   ),
 );
 
-const persistor = persistStore(store);
-
-export { store, persistor };
+export default store;
